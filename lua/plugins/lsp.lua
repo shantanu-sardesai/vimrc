2d322ca0a219e3ba2ea6a117e6b776fc824eca47
Shantanu Sardesai <shantanu.s.sardesai@gmail.com>
2025-06-05 11:25:15 +0530

    feat: add LSP shortcuts

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -14,11 +14,39 @@ return {
     },
 	},
 	config = function()
+    local nvim_map = vim.api.nvim_set_keymap
+    local nvim_buf_map = vim.api.nvim_buf_set_keymap
+    local opts = { noremap=true, silent=true }
+
+    nvim_map('n', '<leader>d', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+    nvim_map('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+    nvim_map('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+
+    local on_attach = function(_client, bufnr)
+      nvim_buf_map(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+      nvim_buf_map(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+      nvim_buf_map(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+      nvim_buf_map(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+      nvim_buf_map(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+      nvim_buf_map(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+      nvim_buf_map(bufnr, 'n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+    end
 		local lspconf = require("lspconfig")
-    
+
     lspconf.lua_ls.setup({})
     lspconf.clojure_lsp.setup({})
-    lspconf.clangd.setup({})
+    lspconf.clangd.setup({
+      on_attach = on_attach,
+      init_options = {
+        compilationDatabaseDirectory = "build";
+        index = {
+          threads = 0;
+        };
+        clang = {
+          excludeArgs = {};
+        };
+      }
+    })
     lspconf.ts_ls.setup({}) -- typescript language server.
 	end,
 }
