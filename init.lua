1fe05a70ae942406ca0a2c8cdb36221365f96478
Shantanu Sardesai <shantanu.s.sardesai@gmail.com>
2025-06-10 15:22:56 +0530

    feat: properly configure each colorscheme

--- init.lua
+++ init.lua
@@ -1,5 +1,3 @@
 require("config.options")
 require("config.lazy")
 require("config.keymaps")
-
-vim.cmd.colorscheme("habamax.nvim")
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,19 +3,56 @@
 --  https://dotfyle.com/neovim/colorscheme/trending
 --  https://nvchad.com/themes/
 
+local set_theme = vim.cmd.colorscheme
+
 local gruvbox = { "ellisonleao/gruvbox.nvim" }
-local github = { "projekt0n/github-nvim-theme", name = "github-theme" } -- https://github.com/projekt0n/github-nvim-theme
+local github = {
+  "projekt0n/github-nvim-theme",
+  name = "github-theme",
+  config = function ()
+    set_theme("github_dark_dimmed")
+  end,
+} -- https://github.com/projekt0n/github-nvim-theme
 local okai = { "okaihe/okai" } -- https://github.com/okaihe/okai
 local vesper = { "datsfilipe/vesper.nvim" } -- https://github.com/datsfilipe/vesper.nvim
 local min = { "datsfilipe/min-theme.nvim" } -- https://github.com/datsfilipe/min-theme.nvim
 local tokyo_dark = { "tiagovla/tokyodark.nvim" } -- https://github.com/tiagovla/tokyodark.nvim
 local catppuccin = { "catppuccin/nvim" } -- https://github.com/catppuccin/nvim
 local kanagawa = { "rebelot/kanagawa.nvim" } -- https://github.com/rebelot/kanagawa.nvim
-local ayu = { "ayu-theme/ayu-vim" } -- https://github.com/ayu-theme/ayu-vim
-local darcula_dark = { "xiantang/darcula-dark.nvim" } -- https://github.com/xiantang/darcula-dark.nvim?tab=readme-ov-file
-local darcula = { "doums/darcula" } -- https://github.com/doums/darcula
+local ayu = {
+  "ayu-theme/ayu-vim",
+  config = function ()
+    set_theme("ayu")
+  end
+} -- https://github.com/ayu-theme/ayu-vim
+local darcula_dark = {
+  "xiantang/darcula-dark.nvim",
+  config = function ()
+    require("darcula").setup({
+      opt = {
+				integrations = {
+					telescope = false,
+				},
+			},
+    })
+
+    set_theme("darcula-dark")
+  end,
+} -- https://github.com/xiantang/darcula-dark.nvim?tab=readme-ov-file
+local darcula = {
+  "doums/darcula",
+  config = function ()
+    set_theme("darcula")
+  end
+} -- https://github.com/doums/darcula
 local bamboo = { "ribru17/bamboo.nvim" } -- https://github.com/ribru17/bamboo.nvim
 local nordic = { "AlexvZyl/nordic.nvim" } -- https://github.com/AlexvZyl/nordic.nvim
-local habamax = { "ntk148v/habamax.nvim", dependencies = { "rktjmp/lush.nvim" } } -- https://github.com/ntk148v/habamax.nvim
+local habamax = {
+  "ntk148v/habamax.nvim",
+  dependencies = { "rktjmp/lush.nvim" },
+  config = function ()
+    set_theme("habamax.nvim")
+  end
+} -- https://github.com/ntk148v/habamax.nvim
 
-return habamax
+return darcula
