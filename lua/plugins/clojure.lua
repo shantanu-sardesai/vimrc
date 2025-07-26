49c57239d7ff7a0f7d6a577fba02baed0ac3a37e
Shantanu Sardesai <shantanu.s.sardesai@gmail.com>
2025-07-26 18:58:26 +0530

    chore: format

--- lua/plugins/clojure.lua
+++ lua/plugins/clojure.lua
@@ -4,23 +4,23 @@ return {
 		event = "InsertEnter",
 		config = true,
 	},
-	{ -- Colorizes the REPL buffer.
-		-- not configured to work with Conjure properly, configuration docs:
-		--   https://github.com/m00qek/baleia.nvim?tab=readme-ov-file#with-conjure
-		"m00qek/baleia.nvim",
-		version = "*",
-		config = function()
-			vim.g.baleia = require("baleia").setup({})
-
-			-- Command to colorize the current buffer
-			vim.api.nvim_create_user_command("BaleiaColorize", function()
-				vim.g.baleia.once(vim.api.nvim_get_current_buf())
-			end, { bang = true })
-
-			-- Command to show logs
-			vim.api.nvim_create_user_command("BaleiaLogs", vim.g.baleia.logger.show, { bang = true })
-		end,
-	},
+	-- { -- Colorizes the REPL buffer.
+	-- 	-- not configured to work with Conjure properly, configuration docs:
+	-- 	--   https://github.com/m00qek/baleia.nvim?tab=readme-ov-file#with-conjure
+	-- 	"m00qek/baleia.nvim",
+	-- 	version = "*",
+	-- 	config = function()
+	-- 		vim.g.baleia = require("baleia").setup({})
+	--
+	-- 		-- Command to colorize the current buffer
+	-- 		vim.api.nvim_create_user_command("BaleiaColorize", function()
+	-- 			vim.g.baleia.once(vim.api.nvim_get_current_buf())
+	-- 		end, { bang = true })
+	--
+	-- 		-- Command to show logs
+	-- 		vim.api.nvim_create_user_command("BaleiaLogs", vim.g.baleia.logger.show, { bang = true })
+	-- 	end,
+	-- },
 	{ "Olical/conjure" }, -- Primary Clojure plugin.
 	{ -- Plugin for enabling structural editing.
 		"julienvincent/nvim-paredit",
