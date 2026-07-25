ad76233158a56d7ddffdfd9a6219bacfadee1a78
Shantanu Sardesai <hi.shantanu@sardesai.dev>
2026-07-25 12:22:15 +0530

    Enable untracked & ignored files view

--- lua/plugins/files.lua
+++ lua/plugins/files.lua
@@ -53,16 +53,12 @@ return {
 		"folke/snacks.nvim",
 		---@type snacks.Config
 		opts = {
-			explorer = {
-				-- your explorer configuration comes here
-				-- or leave it empty to use the default settings
-				-- refer to the configuration section below
-			},
+			explorer = {},
 			picker = {
 				sources = {
 					explorer = {
-						-- your explorer picker configuration comes here
-						-- or leave it empty to use the default settings
+						hidden = true,
+						ignored = true,
 					},
 				},
 			},
